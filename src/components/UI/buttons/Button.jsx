93361fbfab153a5368e56f8f9c874bf93890d8f7
Aiylchieva Rabiya <rabiyaaiylchieva@gmail.com>
2024-01-12 18:43:40 +0600

    added an variant to the button

--- src/components/UI/buttons/Button.jsx
+++ src/components/UI/buttons/Button.jsx
@@ -2,8 +2,17 @@ import { forwardRef } from 'react'
 import { styled, Button as MuiButton } from '@mui/material'
 
 const Button = forwardRef(
-   ({ onClick, disabled, children, icon, ...rest }, ref) => (
-      <StyledButton onClick={onClick} disabled={disabled} ref={ref} {...rest}>
+   (
+      { onClick, disabled, children, icon, variant = 'normal', ...rest },
+      ref
+   ) => (
+      <StyledButton
+         onClick={onClick}
+         disabled={disabled}
+         variant={variant}
+         ref={ref}
+         {...rest}
+      >
          {icon && <span className="icon">{icon}</span>}
          {children}
       </StyledButton>
@@ -11,34 +20,107 @@ const Button = forwardRef(
 )
 export default Button
 
-const StyledButton = styled(MuiButton)(({ theme }) => ({
-   '&.MuiButton-root': {
-      borderRadius: '0.5rem',
-      background: theme.palette.primary.main,
-      padding: '0.813rem 1.5rem',
-      boxShadow: 'none',
-      color: '#FFF',
-      textAlign: 'center',
-      fontSize: '0.875rem',
-      fontStyle: 'normal',
-      fontWeight: '400',
-      lineHeight: 'normal',
-      '&:hover': {
-         borderRadius: '0.5rem',
-         background: '#4E28E8',
-      },
-      '&:active': {
-         background: '#3007DA',
-      },
-      '&.Mui-disabled': {
-         opacity: 0.5,
-         background: 'white',
-         color: theme.palette.primary.white,
-         backgroundColor: '#C4C4C4',
-         border: '0.125rem solid',
-      },
-      '& .icon': {
-         marginRight: '0.625rem',
-      },
-   },
-}))
+const StyledButton = styled(MuiButton)(({ theme, variant }) => {
+   if (variant === 'normal') {
+      return {
+         '&.MuiButton-root': {
+            borderRadius: '0.5rem',
+            height: '2.625rem',
+            background: theme.palette.primary.main,
+            padding: '0.813rem 1.5rem',
+            color: theme.palette.primary.white,
+            textAlign: 'center',
+            fontSize: '0.875rem',
+            fontStyle: 'normal',
+            fontWeight: '400',
+            lineHeight: 'normal',
+            boxShadow:
+               '0rem 0.063rem 0.125rem 0rem rgba(76, 72, 89, 0.20), 0rem 0.063rem 0.125rem 0rem rgba(76, 72, 89, 0.20)',
+            '&:hover': {
+               background: '#4E28E8',
+            },
+            '&:active': {
+               background: '#3007DA',
+            },
+            '&.Mui-disabled': {
+               opacity: 0.5,
+               background: 'white',
+               color: theme.palette.primary.white,
+               backgroundColor: '#C4C4C4',
+               border: '0.125rem solid #C4C4C4',
+            },
+            '& .icon': {
+               marginRight: '0.625rem',
+               textAlign: 'center',
+            },
+         },
+      }
+   }
+   if (variant === 'primary') {
+      return {
+         '&.MuiButton-root': {
+            borderRadius: '0.5rem',
+            height: '2.625rem',
+            background: 'var(--Green-button, #2AB930)',
+            padding: '0.813rem 1.5rem',
+            color: theme.palette.primary.white,
+            textAlign: 'center',
+            fontSize: '0.875rem',
+            fontStyle: 'normal',
+            fontWeight: '400',
+            lineHeight: 'normal',
+            boxShadow:
+               '0rem 0.063rem 0.125rem 0rem rgba(76, 72, 89, 0.20), 0rem 0.063rem 0.125rem 0rem rgba(76, 72, 89, 0.20)',
+            '&:hover': {
+               background: '#31CF38',
+            },
+            '&:active': {
+               background: '#08AF10',
+            },
+            '&.Mui-disabled': {
+               opacity: 0.5,
+               background: 'white',
+               color: '#C4C4C4',
+               border: '0.125rem solid',
+            },
+         },
+      }
+   }
+   if (variant === 'secondary') {
+      return {
+         '&.MuiButton-root': {
+            borderRadius: '0.5rem',
+            height: '2.625rem',
+            background: theme.palette.primary.white,
+            padding: '0.813rem 1.5rem',
+            color: theme.palette.primary.main,
+            borderColor: theme.palette.primary.white,
+            border: '1px solid',
+            textAlign: 'center',
+            fontSize: '0.875rem',
+            fontStyle: 'normal',
+            fontWeight: '400',
+            lineHeight: 'normal',
+            boxShadow:
+               '0rem 0.063rem 0.125rem 0rem rgba(76, 72, 89, 0.20), 0rem 0.063rem 0.125rem 0rem rgba(76, 72, 89, 0.20)',
+            '&:hover': {
+               borderColor: theme.palette.primary.main,
+               background: theme.palette.primary.main,
+               color: theme.palette.primary.white,
+            },
+            '&:active': {
+               borderColor: theme.palette.primary.main,
+               color: theme.palette.primary.white,
+               background: '#3007DA',
+            },
+            '&.Mui-disabled': {
+               opacity: 0.5,
+               background: 'white',
+               color: '#C4C4C4',
+               border: '0.125rem solid',
+            },
+         },
+      }
+   }
+   return {}
+})
--- src/components/UI/buttons/LandingButtons.jsx
+++ src/components/UI/buttons/LandingButtons.jsx
@@ -4,29 +4,23 @@ import BeginBtnImage from '../../../assets/images/buttons/toBegin.png'
 import StartBtnImage from '../../../assets/images/buttons/getStarted.png'
 
 const LandingButtons = forwardRef(
-   ({ children, onClick, isStart, toggleBtn, ...rest }, ref) => (
+   ({ children, onClick, isStart = true, ...rest }, ref) => (
       <div>
-         {isStart ? (
-            <StyleStartButton
-               onClick={onClick}
-               ref={ref}
-               toggleBtn={toggleBtn}
-               {...rest}
-            />
-         ) : (
-            <StyleJoinButton onClick={onClick} ref={ref} {...rest}>
-               {children}
-            </StyleJoinButton>
-         )}
+         <StyleStartButton
+            onClick={onClick}
+            isstart={isStart.toString()}
+            ref={ref}
+            {...rest}
+         />
       </div>
    )
 )
 
 export default LandingButtons
 
-const StyleStartButton = styled(Button)(({ toggleBtn }) => ({
+const StyleStartButton = styled(Button)(({ isstart }) => ({
    backgroundImage: `url(${
-      toggleBtn === 'true' ? StartBtnImage : BeginBtnImage
+      isstart !== 'false' ? StartBtnImage : BeginBtnImage
    })`,
    backgroundSize: 'cover',
    backgroundRepeat: 'no-repeat',
@@ -34,25 +28,5 @@ const StyleStartButton = styled(Button)(({ toggleBtn }) => ({
    height: '3.75rem',
    cursor: 'pointer',
    color: '#eeb6c0',
-}))
-
-const StyleJoinButton = styled(Button)(({ theme }) => ({
-   '&.MuiButton-root': {
-      height: '2.625rem',
-      padding: '0.813rem 1.5rem',
-      borderRadius: '0.5rem',
-      background: theme.palette.primary.main,
-      boxShadow:
-         '0rem 0.063rem 0.125rem 0rem rgba(76, 72, 89, 0.20), 0rem 0.063rem 0.125rem 0rem rgba(76, 72, 89, 0.20)',
-      color: theme.palette.primary.white,
-      textAlign: 'center',
-      fontSize: '0.875rem',
-      fontWeight: 'bold',
-      '&:hover': {
-         background: '#4E28E8',
-      },
-      '&:active': {
-         background: '#3007DA',
-      },
-   },
+   borderRadius: '2rem',
 }))
