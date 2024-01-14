8c5241308f803e9b94ea32eaebd4b0a1d0947b05
IsaevNurislam <rikibiris@gmail.com>
2024-01-14 17:25:33 +0600

    complete 50%

--- src/layout/MainPart3.jsx
+++ src/layout/MainPart3.jsx
@@ -1,48 +1,52 @@
 import React from 'react'
 import { Typography, styled } from '@mui/material'
+import {
+   AccessibleIcon,
+   ExtensiveIcon,
+   SpeechIcon,
+   TutoringIcon,
+} from '../assets/icons'
 
 const MainPart3 = () => {
    return (
       <StyledMainPage>
          <StyledMainContainer>
             <StyledHeaderBlock>
-               <Typography variant="h3">
-                  Unparalleled user experience
-               </Typography>
-               <StyledText variant="p">
+               <StyledH3 variant="h3">Unparalleled user experience</StyledH3>
+               <StyledParagraph variant="p">
                   The most effective way to perfect a language is by immersing
                   yourself in it. Rosetta Stone for Enterprise delivers an
                   effective end-to-end experience, founded on a wealth of
                   carefully structured content. Each learner has the opportunity
                   to balance independent study with optional online tutoring in
                   a way that fits their schedule and language learning goals.
-               </StyledText>
+               </StyledParagraph>
             </StyledHeaderBlock>
 
             <StyledBlockExperience>
                <StyledBlockCon>
                   <StyledCon>
-                     <img alt="network-mobileTime" />
+                     <AccessibleIcon />
                      <Typography variant="p">
                         Accessible anytime, anywhere
                      </Typography>
                   </StyledCon>
                   <StyledCon>
-                     <img alt="search" />
+                     <SpeechIcon />
                      <Typography variant="p">
-                        Extensive business conten
+                        Extensive business content
                      </Typography>
                   </StyledCon>
                </StyledBlockCon>
                <StyledBlockCon>
                   <StyledCon>
-                     <img alt="speech" />
+                     <TutoringIcon />
                      <Typography variant="p">
                         Leading speech recognition
                      </Typography>
                   </StyledCon>
                   <StyledCon>
-                     <img alt="message" />
+                     <ExtensiveIcon />
                      <Typography variant="p">
                         Unlimited live tutoring
                      </Typography>
@@ -51,7 +55,7 @@ const MainPart3 = () => {
             </StyledBlockExperience>
          </StyledMainContainer>
          <StyledMainContainer2>
-            <img alt="placeholder" />
+            <img src="placeholder-image-url" alt="placeholder" />
          </StyledMainContainer2>
       </StyledMainPage>
    )
@@ -59,53 +63,55 @@ const MainPart3 = () => {
 
 export default MainPart3
 
-const StyledHeaderBlock = styled('div')({
-   width: '100%',
+const StyledHeaderBlock = styled('div')(() => ({
    display: 'flex',
    flexDirection: 'column',
    gap: '34px',
-})
+}))
 
+const StyledH3 = styled(Typography)(({ theme }) => ({
+   color: theme.palette.primary.dullBlue,
+   fontSize: '40px',
+   fontWeight: '700',
+   width: '340px',
+}))
+
+const StyledParagraph = styled(Typography)(({ theme }) => ({
+   color: theme.palette.primary.blackGrey,
+   width: '610px',
+   fontWeight: '400',
+   marginTop: '34px',
+}))
 const StyledMainPage = styled('div')({
-   width: '100%',
    display: 'flex',
    flexDirection: 'row',
    justifyContent: 'center',
    alignItems: 'center',
 })
-const StyledMainContainer = styled('div')({
-   width: '100%',
-})
-const StyledText = styled(Typography)({
-   width: '100%',
-   marginTop: '34px',
-})
-const StyledMainContainer2 = styled('div')({
-   width: '100%',
-})
+
+const StyledMainContainer = styled('div')({})
+
+const StyledMainContainer2 = styled('div')({})
+
 const StyledBlockExperience = styled('div')({
-   width: '100%',
    marginTop: '43px',
    display: 'flex',
    gap: '74px',
    flexDirection: 'row',
 })
-const StyledCon = styled('div')({
-   width: '100%',
+
+const StyledCon = styled('div')(({ theme }) => ({
    display: 'flex',
    gap: '26px',
    '& .MuiTypography-root': {
       width: '160px',
-      color: '#23212A',
-      fontFamily: 'Poppins',
-      fontSize: '16px',
-      fontStyle: 'normal',
+      color: theme.palette.primary.blackGrey,
       fontWeight: '400',
       lineHeight: '140%',
    },
-})
+}))
+
 const StyledBlockCon = styled('div')({
-   width: '100%',
    display: 'flex',
    flexDirection: 'column',
    gap: '53px',
