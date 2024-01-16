2a3977720e87fe835ad2e2b43599dcc43f313fbd
Elaman Janyshbekov <elamanzanysbekov4@gmail.com>
2024-01-16 21:37:32 +0600

    moved the array and renamed img

--- src/components/TeamList.jsx
+++ src/components/TeamList.jsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import TeamMember from './TeamMember'
-import teamMembersData from '../utils/constants/TeamMembers'
+import teamMembersData from '../utils/constants/TeamMembersData'
 
 const settings = {
    infinite: true,
