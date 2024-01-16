6a7d16771a83595618f17ffbd0cb7c5e5e365cc3
Meerim Kerkibasheva <mkerkibasheva@gmail.com>
2024-01-16 16:19:12 +0600

    landing main page part six

--- src/components/landing/LearnMore.jsx
+++ src/components/landing/LearnMore.jsx
@@ -24,137 +24,127 @@ const LearnMore = () => {
    }, [])
 
    return (
-      <StyledContainer>
-         <StyledTypography>Learn More</StyledTypography>
-         <StyledAnimationDiv
-            initial={{ clipPath: 'inset(0% 0% 100% 0%)' }}
-            animate={{ clipPath: 'inset(0% 0% 0% 0%)' }}
-            transition={{ duration: 3, ease: 'easeInOut', repeat: Infinity }}
-         >
-            <StyledPathImage />
-         </StyledAnimationDiv>
-
-         <StyledRow>
-            <div data-aos="fade-right">
-               <StyledTypographyH3>
-                  Expand your applicant pool
-               </StyledTypographyH3>
-               <StyledTypographyMoreText>
-                  Tap into a diverse pool of candidates from 210+ countries and
-                  territories of origin, who have taken the Bilingual English
-                  Test because of its radical accessibility.
-               </StyledTypographyMoreText>
-            </div>
-            <StyledAnimationImage
-               initial={{ opacity: 0 }}
-               animate={{ opacity: 1, scale: [0.97, 1, 0.97, 1] }}
-               transition={{ repeat: Infinity, duration: 8 }}
+      <StyledLearnMorePage>
+         <StyledContainer>
+            <StyledTypography>Learn More</StyledTypography>
+            <StyledAnimationDiv
+               initial={{ clipPath: 'inset(0% 0% 100% 0%)', opacity: 0 }}
+               animate={{ clipPath: 'inset(0% 0% 0% 0%)', opacity: 1 }}
+               transition={{ ease: 'easeInOut', repeat: Infinity, duration: 6 }}
             >
-               <GlobeImage />
-            </StyledAnimationImage>
-         </StyledRow>
+               <StyledPathImage />
+            </StyledAnimationDiv>
 
-         <StyledRow>
-            <motion.div
-               initial={{ opacity: 0 }}
-               animate={{ opacity: 1, scale: [0.99, 1, 0.99, 1] }}
-               transition={{ repeat: Infinity, duration: 8 }}
-            >
-               <StyledImageOne />
-            </motion.div>
+            <StyledRow>
+               <div data-aos="fade-right">
+                  <StyledTypographyH3>
+                     Expand your applicant pool
+                  </StyledTypographyH3>
+                  <StyledTypographyMoreText>
+                     Tap into a diverse pool of candidates from 210+ countries
+                     and territories of origin, who have taken the Bilingual
+                     English Test because of its radical accessibility.
+                  </StyledTypographyMoreText>
+               </div>
+               <StyledAnimationImage>
+                  <GlobeImage />
+               </StyledAnimationImage>
+            </StyledRow>
 
-            <div data-aos="fade-left">
-               <StyledTypographyH3>
-                  Built on the latest assessment sciencee
-               </StyledTypographyH3>
-               <StyledTypographyMoreText>
-                  The Duolingo English Test is a computer adaptive test backed
-                  by rigorous research, with results that are highly correlated
-                  <br />
-                  with other major assessments such as the TOEFL and the
-                  <br />
-                  IELTS.
-               </StyledTypographyMoreText>
-            </div>
-         </StyledRow>
+            <StyledRow>
+               <div>
+                  <StyledImageOne />
+               </div>
 
-         <StyledRow>
-            <div data-aos="fade-right">
-               <StyledTypographyH3>Innovative test security</StyledTypographyH3>
-               <StyledTypographyMoreText>
-                  Industry-leading security protocols, individual test
-                  proctoring, and computer adaptive technology help prevent
-                  fraud and cheating and ensure results you can trust.
-               </StyledTypographyMoreText>
-            </div>
-            <motion.div
-               initial={{ opacity: 0 }}
-               animate={{ opacity: 1, scale: [0.99, 1, 0.99, 1] }}
-               transition={{ repeat: Infinity, duration: 8 }}
-            >
-               <StyledImageTwo />
-            </motion.div>
-         </StyledRow>
+               <div data-aos="fade-left">
+                  <StyledTypographyH3>
+                     Built on the latest assessment sciencee
+                  </StyledTypographyH3>
+                  <StyledTypographyMoreText>
+                     The Duolingo English Test is a computer adaptive test
+                     backed by rigorous research, with results that are highly
+                     correlated
+                     <br />
+                     with other major assessments such as the TOEFL and the
+                     <br />
+                     IELTS.
+                  </StyledTypographyMoreText>
+               </div>
+            </StyledRow>
 
-         <StyledRow>
-            <motion.div
-               initial={{ opacity: 0 }}
-               animate={{ opacity: 1, scale: [0.99, 1, 0.99, 1] }}
-               transition={{ repeat: Infinity, duration: 8 }}
-            >
-               <StyledImageThree />
-            </motion.div>
+            <StyledRow>
+               <div data-aos="fade-right">
+                  <StyledTypographyH3>
+                     Innovative test security
+                  </StyledTypographyH3>
+                  <StyledTypographyMoreText>
+                     Industry-leading security protocols, individual test
+                     proctoring, and computer adaptive technology help prevent
+                     fraud and cheating and ensure results you can trust.
+                  </StyledTypographyMoreText>
+               </div>
+               <div>
+                  <StyledImageTwo />
+               </div>
+            </StyledRow>
 
-            <div data-aos="fade-left">
-               <StyledTypographyH3>
-                  Convenient results dashboard
-               </StyledTypographyH3>
-               <StyledTypographyMoreText>
-                  Access candidates’ certificates, video interviews, and writing
-                  samples through a free and secure dashboard. Quickly and
-                  easily view applicant data with filtering tools.
-               </StyledTypographyMoreText>
-            </div>
-         </StyledRow>
+            <StyledRow>
+               <div>
+                  <StyledImageThree />
+               </div>
 
-         <StyledRow>
-            <div data-aos="fade-right">
-               <StyledTypographyH3>Secure Design</StyledTypographyH3>
-               <StyledTypographyMoreText>
-                  Adaptive test engine dynamically administers test questions
-                  from a database of hundreds of thousands of items. <br />
-                  Someone would have to take the test more than 1,000 times
-                  <br />
-                  to see a question repeated.
-               </StyledTypographyMoreText>
-            </div>
-            <motion.div
-               initial={{ opacity: 0 }}
-               animate={{ opacity: 1, scale: [0.98, 0.99, 0.98, 0.99] }}
-               transition={{ repeat: Infinity, duration: 8 }}
-            >
-               <StyledImageFour />
-            </motion.div>
-         </StyledRow>
-         <StyledButton>
-            <LandingButton />
-         </StyledButton>
-      </StyledContainer>
+               <div data-aos="fade-left">
+                  <StyledTypographyH3>
+                     Convenient results dashboard
+                  </StyledTypographyH3>
+                  <StyledTypographyMoreText>
+                     Access candidates’ certificates, video interviews, and
+                     writing samples through a free and secure dashboard.
+                     Quickly and easily view applicant data with filtering
+                     tools.
+                  </StyledTypographyMoreText>
+               </div>
+            </StyledRow>
+
+            <StyledRow>
+               <div data-aos="fade-right">
+                  <StyledTypographyH3>Secure Design</StyledTypographyH3>
+                  <StyledTypographyMoreText>
+                     Adaptive test engine dynamically administers test questions
+                     from a database of hundreds of thousands of items. <br />
+                     Someone would have to take the test more than 1,000 times
+                     <br />
+                     to see a question repeated.
+                  </StyledTypographyMoreText>
+               </div>
+               <div>
+                  <StyledImageFour />
+               </div>
+            </StyledRow>
+            <StyledButton>
+               <LandingButton />
+            </StyledButton>
+         </StyledContainer>
+      </StyledLearnMorePage>
    )
 }
 
 export default LearnMore
 
+const StyledLearnMorePage = styled('div')({
+   backgroundColor: '#FEF5E8',
+   width: '100%',
+})
+
 const StyledContainer = styled('div')({
    margin: 'auto',
-   backgroundColor: '#FEF5E8',
    display: 'flex',
    flexDirection: 'column',
    alignItems: 'center',
    justifyContent: 'center',
    padding: '2rem',
    position: 'relative',
-   maxWidth: '1600px',
+   maxWidth: '1440px',
    width: '100%',
 })
 
@@ -163,7 +153,7 @@ const StyledRow = styled('div')({
    flexDirection: 'row',
    alignItems: 'center',
    justifyContent: 'space-between',
-   width: '100%',
+   width: '1440px',
    paddingTop: '2rem',
    paddingLeft: '1.5rem',
    paddingRight: '2rem',
@@ -190,7 +180,7 @@ const StyledTypographyMoreText = styled(Typography)({
 })
 
 const StyledButton = styled('div')({
-   marginTop: '8rem',
+   marginTop: '7rem',
 })
 
 const StyledAnimationDiv = styled(motion.div)({
@@ -218,28 +208,28 @@ const StyledAnimationImage = styled(motion.div)({
 
 const StyledImageOne = styled(ImageOne)({
    position: 'absolute',
-   top: '-25',
-   left: '20.8rem',
+   top: '28.8rem',
+   left: '22rem',
    zIndex: 1,
 })
 
 const StyledImageTwo = styled(ImageTwo)({
    position: 'absolute',
-   top: '-50',
-   right: 400,
+   top: '43.5rem',
+   right: '26rem',
    zIndex: 1,
 })
 
 const StyledImageThree = styled(ImageThree)({
    position: 'absolute',
-   top: '-28',
-   left: 320,
+   top: '60rem',
+   left: 365,
    zIndex: 1,
 })
 
 const StyledImageFour = styled(ImageFour)({
    position: 'absolute',
-   top: '-30',
-   right: 340,
+   top: '76rem',
+   right: '24rem',
    zIndex: 1,
 })
