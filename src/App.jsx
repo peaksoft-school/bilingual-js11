93f9f13b8d32d8cab86349fa7ef48ce4d2998c67
IsaevNurislam <rikibiris@gmail.com>
2024-01-26 11:23:51 +0600

    chnage the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
 import './App.css'
-import CheckOut from './components/landing/CheckOut'
-import Feedbacks from './components/landing/Feedbacks'
-import Intro from './components/landing/Intro'
-import Statistics from './components/landing/Statistics'
-import UsefulVideos from './components/landing/UsefulVideos'
-import UserExperience from './components/landing/UserExperience'
 
 const App = () => {
-   return (
-      <div>
-         <Intro />
-
-         <CheckOut />
-
-         <Feedbacks />
-
-         <Statistics />
-
-         <UserExperience />
-
-         <UsefulVideos />
-      </div>
-   )
+   return <div>Bilingual</div>
 }
 
 export default App
