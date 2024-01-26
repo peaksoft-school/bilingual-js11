93e270e878111ce00cc6636d97bc4d1e009c6792
Aiylchieva Rabiya <rabiyaaiylchieva@gmail.com>
2024-01-26 16:49:39 +0600

    fixed team-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css'
+import TeamList from './components/landing/TeamList'
+import Intro from './components/landing/Intro'
+import Statistics from './components/landing/Statistics'
+import CheckOut from './components/landing/CheckOut'
+import UsefulVideos from './components/landing/UsefulVideos'
+import LearnMore from './components/landing/LearnMore'
+import Feedbacks from './components/landing/Feedbacks'
 
 const App = () => {
-   return <div>Bilingual</div>
+   return (
+      <div>
+         {/* Bilingual
+         <br /> */}
+         <Intro />
+         <Statistics />
+         <TeamList />
+         <CheckOut />
+         <UsefulVideos />
+         <LearnMore />
+         <Feedbacks />
+      </div>
+   )
 }
 
 export default App
