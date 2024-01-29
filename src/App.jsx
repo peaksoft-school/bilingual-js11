aced295903fde2bfa32ceb301999bd35bd1f75d1
Aiylchieva Rabiya <rabiyaaiylchieva@gmail.com>
2024-01-29 20:15:10 +0600

    clear App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import './App.css'
-import SignUpPage from './containers/SignUpPage'
-import SignInPage from './containers/SingInPage'
 
 const App = () => {
-   return (
-      <h1>
-         {/* bilingual-js11 */}
-         {/* <br /> */}
-         <SignUpPage />
-         <SignInPage />
-      </h1>
-   )
+   return <h1>bilingual-js11</h1>
 }
 
 export default App
