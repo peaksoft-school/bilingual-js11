bc6d317c819632f1f66ae9395cca7e3b478128e6
Meerim Kerkibasheva <mkerkibasheva@gmail.com>
2024-02-01 19:56:00 +0600

    created store and axios-instance

---
+++ src/configs/axiosInstance.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants'
+
+export const axiosInstance = axios.create({
+   baseURL: BASE_URL,
+   headers: {
+      'Content-Type': 'application/json',
+   },
+})
+
+let storee
+
+export const injectStore = (store) => {
+   storee = store
+}
+
+axios.interceptors.request.use(
+   function (config) {
+      const updateConfig = { ...config }
+
+      const token = storee.getState().login.accessToken
+
+      if (token) {
+         updateConfig.headers.Authorization = `Bearer ${token}`
+      }
+
+      return config
+   },
+   function (error) {
+      return Promise.reject(error)
+   }
+)
+
+axios.interceptors.response.use(
+   function (response) {
+      return Promise.reject(response)
+   },
+   function (error) {
+      return Promise.reject(error)
+   }
+)
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,25 @@
 import ReactDOM from 'react-dom/client'
+import { Provider } from '@reduxjs/toolkit'
 import { StrictMode } from 'react'
 import App from './App'
 import Themes from './components/Themes'
 import Notification from './components/Notification'
 import './index.css'
+import { injectStore } from './configs/axiosInstance'
+import { store } from './store/store'
+
+injectStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
    <StrictMode>
-      <Themes>
-         <Notification />
+      <Provider store={store}>
+         <Themes>
+            <Notification />
 
-         <App />
-      </Themes>
+            <App />
+         </Themes>
+      </Provider>
    </StrictMode>
 )
