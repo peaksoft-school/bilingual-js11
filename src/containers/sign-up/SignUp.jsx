f36e42aec1637a228e1fc6851bf48fc092c83b5b
Aiylchieva Rabiya <rabiyaaiylchieva@gmail.com>
2024-02-05 12:50:14 +0600

    merge

--- src/containers/sign-up/SignUp.jsx
+++ src/containers/sign-up/SignUp.jsx
@@ -24,7 +24,7 @@ const SignUp = () => {
 
    const handleInputFocus = (name) => setFocusedInput(name)
 
-   const onSubmit = (values, { resetForm }) => resetForm()
+   const onSubmit = (_, { resetForm }) => resetForm()
 
    const {
       values,
