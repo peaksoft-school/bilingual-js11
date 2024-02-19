209c9a408dda18e88261617583d75ccdcd50e72e
Meerim Kerkibasheva <mkerkibasheva@gmail.com>
2024-02-19 14:34:49 +0600

    added correct answer

--- src/pages/admin/type/RecordSayingStatement.jsx
+++ src/pages/admin/type/RecordSayingStatement.jsx
@@ -1,12 +1,38 @@
-import { Box, Typography } from '@mui/material'
+import { useState } from 'react'
+import { Box, Typography, styled } from '@mui/material'
 import Input from '../../../components/UI/Input'
 
-const RecordSayingStatement = () => (
-   <Box>
-      <Typography>Statement</Typography>
+const RecordSayingStatement = () => {
+   const [value, setValue] = useState('')
 
-      <Input />
-   </Box>
-)
+   const handleChange = (event) => {
+      setValue(event.target.value)
+   }
+
+   return (
+      <StyledContainer>
+         <Box className="statement">
+            <Typography>Statement</Typography>
+
+            <Input type="text" value={value} onChange={handleChange} />
+         </Box>
+
+         <Box className="correct-answer">
+            <Typography>Correct answer</Typography>
+
+            <Input type="text" value={value} onChange={handleChange} />
+         </Box>
+      </StyledContainer>
+   )
+}
 
 export default RecordSayingStatement
+
+const StyledContainer = styled(Box)(() => ({
+   fontFamily: 'Arial',
+   color: '#4C4859',
+
+   '& .statement': {
+      marginBottom: '1.4rem',
+   },
+}))
