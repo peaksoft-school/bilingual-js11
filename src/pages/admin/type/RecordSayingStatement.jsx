503c1e4b67ce37b0e45d6d9b6053f460f3c875e9
Meerim Kerkibasheva <mkerkibasheva@gmail.com>
2024-02-19 08:01:19 +0600

    created type record saying statement

---
+++ src/pages/admin/type/RecordSayingStatement.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+import Input from '../../../components/UI/Input'
+
+const RecordSayingStatement = () => (
+   <Box>
+      <Typography>Statement</Typography>
+
+      <Input />
+   </Box>
+)
+
+export default RecordSayingStatement
